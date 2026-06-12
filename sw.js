9dd726fd2a1f43e68750e94a92a8470e89eebb42
c4llm3k1ng <janehmann97@web.de>
2026-06-12 15:45:02 +0200

    Fix TDZ: move all firebase/auth lets to top of script

--- sw.js
+++ sw.js
@@ -1,33 +1,9 @@
-const CACHE = 'school-duel-v7';
-const CORE = [
-  './school-duel.html',
-  './manifest.json',
-  './icon.svg'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
+const CACHE = 'school-duel-v8';
+const CORE = ['./school-duel.html','./manifest.json','./icon.svg'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  const isExternal = url.origin !== location.origin;
+  const isExternal = new URL(e.request.url).origin !== location.origin;
   if (isExternal) { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); return; }
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }))
-  );
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => { caches.open(CACHE).then(c => c.put(e.request, res.clone())); return res; })));
 });
